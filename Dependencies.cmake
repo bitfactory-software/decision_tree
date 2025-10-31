37d951d640c34999cace761439544f74791391e7
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-31 20:23:04 +0100

    drop unused dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -12,28 +12,8 @@ function(myproject_setup_dependencies)
     cpmaddpackage("gh:fmtlib/fmt#11.1.4")
   endif()
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.15.2
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
-
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.8.1")
-  endif()
-
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
-  endif()
-
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI@6.0.2")
   endif()
 
   if(NOT TARGET tools::tools)
