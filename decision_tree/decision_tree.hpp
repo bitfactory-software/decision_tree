ce7685294c8a5e2077bcea66211202f133f7e34e
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-11-20 14:21:11 +0100

    struct input,   auto result_counts() const {

--- decision_tree/decision_tree.hpp
+++ decision_tree/decision_tree.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <set>
+#include <map>
 #include <tuple>
 #include <type_traits>
 #include <vector>
@@ -9,34 +10,43 @@
 namespace decision_tree {
 
 template <typename... Values>
-using row_t = std::tuple<Values...>;
+struct input {
+  // types
+  using row_t = std::tuple<Values...>;
+  using rows_t = std::vector<row_t>;
+  using rows_set_t = std::set<row_t>;
+  inline static constexpr std::size_t column_count = std::tuple_size_v<row_t>;
+  inline static constexpr std::size_t predict_column = column_count - 1;
+  using predict_t = std::tuple_element_t<predict_column, row_t>;
 
-template <typename... Values>
-using rows_t = std::vector<row_t<Values...>>;
+  // data
+  rows_t rows;
 
-template <typename... Values>
-using rows_set_t = std::set<row_t<Values...>>;
+  input() = default;
+  input(std::initializer_list<row_t> const& init) : rows(init) {}
 
-template <typename V>
-struct node {
-  V value;
-};
-
-template <std::size_t I, typename V, typename... Values>
-constexpr bool splits(row_t<Values...> const& row, V const& value) {
-  if constexpr (std::is_arithmetic_v<V>) {
-    return std::get<I>(row) >= value;
-  } else {
-    return std::get<I>(row) == value;
+  template <std::size_t I, typename V>
+  static constexpr bool splits(row_t const& row, V const& value) {
+    if constexpr (std::is_arithmetic_v<V>) {
+      return std::get<I>(row) >= value;
+    } else {
+      return std::get<I>(row) == value;
+    }
   }
-}
 
-template <std::size_t I, typename V, typename... Values>
-auto divide_table_column(rows_t<Values...> const& rows, V const& value) {
-  std::array<rows_set_t<Values...>, 2> split_sets;
-  for (auto const& row : rows)
-    split_sets[splits<I>(row, value) ? 0 : 1].insert(row);
-  return split_sets;
-}
+  template <std::size_t I, typename V>
+  auto divide_table_column(V const& value) const {
+    std::array<rows_set_t, 2> split_sets;
+    for (auto const& row : rows)
+      split_sets[splits<I>(row, value) ? 0 : 1].insert(row);
+    return split_sets;
+  }
+
+  auto result_counts() const {
+    std::map<predict_t, int> counts;
+    for (auto const& row : rows) ++counts[std::get<predict_column>(row)];
+    return counts;
+  }
+};
 
 }  // namespace decision_tree
--- test/tests.cpp
+++ test/tests.cpp
@@ -7,33 +7,32 @@ using namespace decision_tree;
 
 namespace {
 // referrer, loaction, read FAQ, pages viewed, service choosen
-using behavior_to_decision_table_t =
-    rows_t<std::string, std::string, bool, int, std::string>;
-behavior_to_decision_table_t rows = {
-    {"Slashdot", "USA", true, 18, "None"},
-    {"Google", "France", true, 23, "Premium"},
-    {"Digg", "USA", true, 24, "Basic"},
-    {"Kiwitobes", "France", true, 23, "Basic"},
-    {"Google", "UK", false, 21, "Premium"},
-    {"(direct)", "New Zealand", false, 12, "None"},
-    {"(direct)", "UK", false, 21, "Basic"},
-    {"Google", "USA", false, 24, "Premium"},
-    {"Slashdot", "France", true, 19, "None"},
-    {"Digg", "USA", false, 18, "None"},
-    {"Google", "UK", false, 18, "None"},
-    {"Kiwitobes", "UK", false, 19, "None"},
-    {"Digg", "New Zealand", true, 12, "Basic"},
-    {"Google", "UK", true, 18, "Basic"},
-    {"Kiwitobes", "France", true, 19, "Basic"}};
+using input_t =
+    decision_tree::input<std::string, std::string, bool, int, std::string>;
+input_t rows{{"Slashdot", "USA", true, 18, "None"},
+             {"Google", "France", true, 23, "Premium"},
+             {"Digg", "USA", true, 24, "Basic"},
+             {"Kiwitobes", "France", true, 23, "Basic"},
+             {"Google", "UK", false, 21, "Premium"},
+             {"(direct)", "New Zealand", false, 12, "None"},
+             {"(direct)", "UK", false, 21, "Basic"},
+             {"Google", "USA", false, 24, "Premium"},
+             {"Slashdot", "France", true, 19, "None"},
+             {"Digg", "USA", false, 18, "None"},
+             {"Google", "UK", false, 18, "None"},
+             {"Kiwitobes", "UK", false, 19, "None"},
+             {"Digg", "New Zealand", true, 12, "Basic"},
+             {"Google", "UK", true, 18, "Basic"},
+             {"Kiwitobes", "France", true, 19, "Basic"}};
 
 }  // namespace
 
 TEST_CASE("divide_table_column") {
-  auto split = divide_table_column<2>(rows, true);
+  auto split = rows.divide_table_column<2>(true);
 
   CHECK(split[0].size() == 8);
   std::println("split[0] {}", split[0]);
-  behavior_to_decision_table_t expected_0 = {
+  input_t::rows_t expected_0 = {
       {"Slashdot", "USA", true, 18, "None"},
       {"Google", "France", true, 23, "Premium"},
       {"Digg", "USA", true, 24, "Basic"},
@@ -47,7 +46,7 @@ TEST_CASE("divide_table_column") {
 
   CHECK(split[1].size() == 7);
   std::println("split[1] {}", split[1]);
-  behavior_to_decision_table_t expected_1 = {
+  input_t::rows_t expected_1 = {
       {"Google", "UK", false, 21, "Premium"},
       {"(direct)", "UK", false, 21, "Basic"},
       {"Google", "USA", false, 24, "Premium"},
@@ -56,4 +55,12 @@ TEST_CASE("divide_table_column") {
       {"Kiwitobes", "UK", false, 19, "None"}};
   for (auto const& row : expected_1)
     CHECK(split[1].find(row) != split[1].end());
+}
+
+TEST_CASE("result_counts") {
+  auto counts = rows.result_counts();
+  CHECK(counts.size() == 3);
+  CHECK(counts["None"] == 6);
+  CHECK(counts["Basic"] == 6);
+  CHECK(counts["Premium"] == 3);
 }
