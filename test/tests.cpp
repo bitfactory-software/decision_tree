dd3dc38f31803ebb2420aeb2b9dfd9a3ce16fe3b
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-11-21 11:01:28 +0100

    remove includes for gcc/clang

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <catch2/matchers/catch_matchers.hpp>
-#include <decision_tree/decision_tree.hpp>
-#include <iostream>
-//#include <print>
-#include <string>
-#include <algorithm>
+//#include <catch2/matchers/catch_matchers_floating_point.hpp>
+//#include <catch2/matchers/catch_matchers.hpp>
+//#include <decision_tree/decision_tree.hpp>
+//#include <iostream>
+////#include <print>
+//#include <string>
+//#include <algorithm>
 
-using namespace Catch::Matchers;
+//using namespace Catch::Matchers;
 
 //namespace {
 //// referrer, loaction, read FAQ, pages viewed, service choosen
