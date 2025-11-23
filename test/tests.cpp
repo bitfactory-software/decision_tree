7517b88dfbac23240c481531d4e15e5761fe0038
Andi <andreas.pfaffenbichler@bitfactory.at>
2025-11-23 19:15:54 +0100

    introduce bit_factory

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <decision_tree/decision_tree.hpp>
+#include <bit_factory/decision_tree.hpp>
 #include <iostream>
 // #include <print>
 #include <algorithm>
