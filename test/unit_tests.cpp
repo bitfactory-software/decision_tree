13e33d919fba832296fcb63c59f8d4c8f5b4205e
andreaspfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-12-03 14:08:47 +0100

    TEST_CASE("build_tree2")

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -33,3 +33,28 @@ F-> 0:?
   CHECK(test({"D", "", "D"}) == "{D: 1}");
   CHECK(test({"", "", ""}) == "{: 1}");
 }
+
+TEST_CASE("build_tree2") {
+  using namespace bit_factory;
+  std::string ___ = "", X = "X", Y = "Y";
+  using decision_tree = ml::decision_tree<
+      std::string, std::string, std::string, std::string, std::string, std::string, std::string, std::string, std::string >;
+  const decision_tree::rows_t samples{
+      {___, ___, ___, ___, ___, ___, X, ___, Y},
+      {___, ___, ___, ___, ___, Y, ___, ___, Y},
+      {___, ___, ___, ___, ___, ___, X, ___, X},
+      {___, ___, ___, ___, ___, Y, ___, ___, ___},
+      {___, ___, ___, ___, ___, ___, X, ___, ___}};
+
+  auto tree = decision_tree::build_tree(samples);
+  CHECK(to_string(tree) ==
+        R"(5:?
+T-> {: 1, X: 1, Y: 1}
+F-> {: 1, Y: 1}
+)");
+
+  auto test = [&](decision_tree::observation_t const& observation) {
+    return decision_tree::to_string(decision_tree::classify(tree, observation));
+  };
+  CHECK(test({___, ___, ___, ___, ___, ___, Y, ___}) == "");
+}
